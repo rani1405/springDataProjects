461086b3bf4b2a8a4a90dd3baf658c4b5a7ce95d
rani1405 <jdpatel029@gmail.com>
2025-02-15 10:51:41 -0500

    main file

---
+++ spring-data-mongoDB/src/main/java/com/example/springboot/mongodb/SpringBootWithMangoDbApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithMangoDbApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithMangoDbApplication.class, args);
+	}
+
+}
