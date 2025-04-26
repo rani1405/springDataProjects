118a4acd64638fd13b75076513af6750c3938467
rani1405 <jdpatel029@gmail.com>
2025-04-26 15:37:32 -0400

    Initial commit- postgreSQL demo

--- spring-data-mongoDB/src/main/java/com/example/springboot/mongodb/SpringBootWithMangoDbApplication.java
+++ spring-data-mongoDB/src/main/java/com/example/springboot/mongodb/SpringBootWithMangoDbApplication.java
@@ -1,12 +1,17 @@
 package com.example.springboot.mongodb;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootWithMangoDbApplication {
+	@Value("${spring.application.name}")
+	private static String appName;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithMangoDbApplication.class, args);
+		System.out.println("appname--"+ appName);
 	}
 
 }
